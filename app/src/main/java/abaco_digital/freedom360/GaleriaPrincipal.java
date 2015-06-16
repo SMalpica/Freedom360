29262420fe78a26e3abec9f5565ba576bbb06e77
SMalpica <sandra_malpica@hotmail.com>
2015-06-16 15:58:50 +0200

    en tablet se ve bien todo

--- app/src/main/java/abaco_digital/freedom360/GaleriaPrincipal.java
+++ app/src/main/java/abaco_digital/freedom360/GaleriaPrincipal.java
@@ -5,22 +5,17 @@ import abaco_digital.freedom360.util.SystemUiHider;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.HorizontalScrollView;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.view.Display;
 import android.widget.TextView;
 import android.graphics.Point;
-import android.graphics.Bitmap;
 
 
 /**
@@ -42,21 +37,6 @@ public class GaleriaPrincipal extends Activity {
      */
     private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
 
-    /**
-     * If set, will toggle the system UI visibility upon interaction. Otherwise,
-     * will show the system UI visibility upon interaction.
-     */
-    private static final boolean TOGGLE_ON_CLICK = false;
-
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
-    private static final int HIDER_FLAGS = SystemUiHider.FLAG_FULLSCREEN;
-
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
-    private SystemUiHider mSystemUiHider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,63 +53,7 @@ public class GaleriaPrincipal extends Activity {
 
 
         contentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE);
-        // This example uses decor view, but you can use any visible view.
 
-        // Set up an instance of SystemUiHider to control the system UI for
-        // this activity.
-        //mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
-        //mSystemUiHider.setup();
-        /*mSystemUiHider
-                .setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-                    // Cached values.
-                    int mControlsHeight;
-                    int mShortAnimTime;
-
-                    @Override
-                    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-                    public void onVisibilityChange(boolean visible) {
-                      /*  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-                            // If the ViewPropertyAnimator API is available
-                            // (Honeycomb MR2 and later), use it to animate the
-                            // in-layout UI controls at the bottom of the
-                            // screen.
-                            if (mControlsHeight == 0) {
-                                mControlsHeight = controlsView.getHeight();
-                            }
-                            if (mShortAnimTime == 0) {
-                                mShortAnimTime = getResources().getInteger(
-                                        android.R.integer.config_shortAnimTime);
-                            }
-                            controlsView.animate()
-                                    .translationY(visible ? 0 : mControlsHeight)
-                                    .setDuration(mShortAnimTime);
-                        } else {
-                            // If the ViewPropertyAnimator APIs aren't
-                            // available, simply show or hide the in-layout UI
-                            // controls.
-                            controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
-                        }
-
-                        if (visible) {
-                            // Schedule a hide().
-                            delayedHide(1);
-                            //delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                        }
-                    }
-                });*/
-
-        // Set up the user interaction to manually show or hide the system UI.
-//        contentView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (TOGGLE_ON_CLICK) {
-//                    mSystemUiHider.hide();
-//                } else {
-//                    //mSystemUiHider.show();
-//                    mSystemUiHider.hide();
-//                }
-//            }
-//        });
 
         //MEDIMOS LA PANTALLA
         Display display = getWindowManager().getDefaultDisplay();
@@ -151,13 +75,6 @@ public class GaleriaPrincipal extends Activity {
         //en xml
         TextView texto = (TextView)findViewById(R.id.editText);
         texto.setMaxHeight(outMetrics.heightPixels * 2 / 6);
-        Drawable editTextDrawable = ((TextView) findViewById(R.id.editText)).getCompoundDrawables()[0];
-
-        //editTextDrawable.setBounds(0, 0, editTextDrawable.getIntrinsicWidth(), editTextDrawable.getIntrinsicHeight());
-        //ImageView camaras = (ImageView)findViewById(R.id.imageView);
-        //BitmapDrawable imagen = (BitmapDrawable)camaras.getDrawable();
-        //int altura = imagen.getBitmap().getHeight();
-        //texto.getLayoutParams().height=altura;
     }
 
     @Override
@@ -171,20 +88,6 @@ public class GaleriaPrincipal extends Activity {
     }
 
 
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
 
     Handler mHideHandler = new Handler();
     Runnable mHideRunnable = new Runnable() {
